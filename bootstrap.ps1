8b8c5b83feaa3064a3acf6d4cb43224b5670ed31
Chris Grimmett <chris@grimtech.net>
2019-01-11 12:47:04 -0800

    Update bootstrap.ps1

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,6 +1,5 @@
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install -y steam
-choco install -y notepadplusplus
 choco install -y synergy
 choco install -y zerotier-one
 choco install -y steamcmd

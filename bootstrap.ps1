796413f9bdfa20b68f15908cb4ad64f007eab68f
Chris Grimmett <chris@grimtech.net>
2019-01-11 09:44:46 -0800

    add admin privs

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,2 +1,4 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
 . { iwr -useb https://boxstarter.org/bootstrapper.ps1 } | iex; get-boxstarter -Force
 Install-BoxstarterPackage -PackageName https://github.com/insanity54/boxstarter-test/raw/master/provision.txt -DisableReboots

249d0e1c1c1b3364b3d1451dfe9b40700273a4af
Chris Grimmett <chris@grimtech.net>
2019-01-11 13:58:15 -0800

    Update steamprep.ps1

--- steamprep.ps1
+++ steamprep.ps1
@@ -2,6 +2,6 @@ choco install -y steam
 choco install -y synergy
 choco install -y zerotier-one
 choco install -y steamcmd
-
+choco install -y geforce-game-ready-driver
 steamcmd +login anonymous
 steamcmd +app_update 393380

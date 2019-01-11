e6a7125e4c9ae60e1b02d04848684e24f38a5169
Chris Grimmett <chris@grimtech.net>
2019-01-11 12:38:00 -0800

    Update bootstrap.ps1

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,10 +1,9 @@
-. { iwr -useb https://boxstarter.org/bootstrapper.ps1 } | iex; get-boxstarter -Force -Version=2.10.3
-
-cinst steam
-cinst notepadplusplus
-cinst synergy
-cinst zerotier-one
-cinst steamcmd
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install -y steam
+choco install -y notepadplusplus
+choco install -y synergy
+choco install -y zerotier-one
+choco install -y steamcmd
 
 steamcmd +login anonymous
 steamcmd +app_update 393380

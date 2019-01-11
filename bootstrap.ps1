eb76283c947b628b8febec8fd1ef323ac4808bbb
Chris Grimmett <chris@grimtech.net>
2019-01-11 11:37:23 -0800

    Update bootstrap.ps1

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,2 +1,10 @@
 . { iwr -useb https://boxstarter.org/bootstrapper.ps1 } | iex; get-boxstarter -Force
-Install-BoxstarterPackage -PackageName https://github.com/insanity54/boxstarter-test/raw/master/provision.txt -DisableReboots
+
+cinst steam
+cinst notepadplusplus
+cinst synergy
+cinst zerotier-one
+cinst steamcmd
+
+steamcmd +login anonymous
+steamcmd +app_update 393380

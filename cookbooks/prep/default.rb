ec8369aec9494091edd0fbb81f63b48be3ee09b1
Chris Grimmett <chris@grimtech.net>
2019-01-11 13:38:46 -0800

    Update default.rb

--- cookbooks/prep/default.rb
+++ cookbooks/prep/default.rb
@@ -5,3 +5,13 @@ dsc_resource 'useradd' do
   property :password, ps_credential('P@assword!')
   property :ensure, 'present'
 end
+
+
+chocolatey_package 'geforce-game-ready-driver' do
+  action :install
+end
+
+remote_file "#{ENV['HOME']}\\Downloads\parsec-windows32.exe" do
+  source 'https://s3.amazonaws.com/parsec-build/package/parsec-windows32.exe'
+end
+

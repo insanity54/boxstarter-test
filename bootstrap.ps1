a372cb5084085be55e30d7db5dbe2c1e7d173267
Chris Grimmett <chris@grimtech.net>
2019-01-11 13:59:29 -0800

    Update bootstrap.ps1

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,3 +1,8 @@
+# install choco package manager
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 
+# download prep file
 Invoke-RestMethod -Uri https://github.com/insanity54/boxstarter-test/raw/master/steamprep.ps1 -Method Get -OutFile C:steamprep.ps1
+
+# download parsec
+Invoke-RestMethod -Uri https://s3.amazonaws.com/parsec-build/package/parsec-windows32.exe -Method Get -OutFile C:parsec-windows32.exe

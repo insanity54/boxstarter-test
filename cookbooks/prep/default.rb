ebacdd5588695bf9d3793edc5a30c270a40ced0f
Chris Grimmett <chris@grimtech.net>
2019-01-11 13:51:11 -0800

    Update default.rb

--- cookbooks/prep/default.rb
+++ cookbooks/prep/default.rb
@@ -11,7 +11,20 @@ chocolatey_package 'geforce-game-ready-driver' do
   action :install
 end
 
+chocolatey_package 'steam' do
+  action :install
+end
+
+chocolatey_package 'steamcmd' do
+  action :install
+end
+
 remote_file "#{ENV['HOME']}\\Downloads\parsec-windows32.exe" do
   source 'https://s3.amazonaws.com/parsec-build/package/parsec-windows32.exe'
 end
 
+batch 'Update steam' do
+  code <<-EOH
+    steamcmd +login anonymous
+    steamcmd +app_update 393380
+end
